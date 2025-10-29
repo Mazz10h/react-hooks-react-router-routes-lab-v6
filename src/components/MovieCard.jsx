52ae7894a0fefa71cc949613f32c532d02755dea
Fainus Gabriel <nusgabriel17@gmail.com>
2025-10-29 10:47:49 +0300

    changes

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,21 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-function MovieCard({title}) {
-  
+function MovieCard({ movie }) {
   return (
     <article>
-        <h2>{title}</h2>
-        {/* What should go here? */}
+      <h2>{movie.title}</h2>
+      <Link to={`/movies/${movie.id}`}>View Info</Link>
     </article>
   );
+}
+
+// ✅ PropTypes validation
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default MovieCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,30 @@
+// src/pages/Home.jsx
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Home() {
-  return (
-    <>
-      <header>
-        {/* What component should go here? */}
-      </header>
-      <main>
-        {/* Info goes here! */}
-      </main>
-    </>
-  );
-};
+  const [movies, setMovies] = useState([]);
 
-export default Home;
+  useEffect(() => {
+    fetch("http://localhost:3000/movies")
+      .then((res) => res.json())
+      .then((data) => setMovies(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <NavBar/>
+      <h1>Home Page</h1>
+      {movies.map((movie) => (
+        <article key={movie.id}>
+          <h2>{movie.title}</h2>
+<Link to={`/movie/${movie.id}`}>View Info</Link>
+        </article>
+      ))};
+    </div>
+  );
+}
+
+export default Home
